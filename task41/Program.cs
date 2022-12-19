4789f7a8d4d8daa2d28a2b70386d0279a19cb073
Dmitry <panfilovdima1@yandex.ru>
2022-12-19 23:35:03 +0300

    HW6 done

---
+++ task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Укажите количество вводимых чисел");
+int numAmount = int.Parse(Console.ReadLine()!);
+
+int[] arr = new int[numAmount];
+
+UserFillArray(arr);
+
+PrintArray(arr);
+
+int positivNumAmount = PositivNumCount(arr);
+
+Console.WriteLine("Количество положительных чисел = " + positivNumAmount);
+
+int PositivNumCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    { 
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] UserFillArray(int[] array)
+{
+    for (int i = 1; i < array.Length + 1; i++)
+    {
+        Console.WriteLine("Введите число под номером " + i + ":");
+        array[i-1] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}    
---
+++ task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите значение k1: ");
+int firstKoef = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение b1: ");
+int firstFreeKoef = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение k2: ");
+int secondKoef = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите значение b2: ");
+int secondFreeKoef = int.Parse(Console.ReadLine()!);
+
+double[] arr = InterectionPointSearch(firstKoef, firstFreeKoef, secondKoef, secondFreeKoef);
+
+Console.WriteLine("Координаты точки пересечения заданных прямых: ");
+
+Console.WriteLine("(" + arr[0] + " " + arr[1] + ")");
+
+double[] InterectionPointSearch (int A, int B, int C, int D)
+{
+    double[] array = new double[2];
+
+    array[0] = Math.Round((double)(B - D) * (-1) / (A - C),1);
+    array[1] = Math.Round((C * array[0] + D), 1);
+    
+    return array;
+}
